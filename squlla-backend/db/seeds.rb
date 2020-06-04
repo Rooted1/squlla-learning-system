03b2150c8a944e71fe5a3c5b8b9e1516ad3544e9
Flatiron Staff <flatironschool@Flatirons-MacBook-Air.local>
2020-06-04 12:34:53 -0700

    create rails backend with migrations and seed

---
+++ squlla-backend/db/seeds.rb
@@ -0,0 +1,112 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Flashcard.create({
+    question: "Displacement",
+    answer: "Change in position of an object",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Average Speed",
+    answer: "Total distance divided by the total time travelled",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Instantaneous speed",
+    answer: "The speed of an object at a particular instant in time",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Velocity",
+    answer: "The rate of change of position of a body in a specific direction",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Acceleration",
+    answer: "The rate of the velocity of an object with respect to time",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Center of Gravity",
+    answer: "The point from which the weight of a body or system is acting. Also known as Center of Mass",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Torque",
+    answer: "The measure of the force that can cause an object to rotate about an axis",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Workdone",
+    answer: "The measure of energy transferred by a force acting through a distance",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Energy",
+    answer: "The ability to do work",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Power",
+    answer: "The rate at which work is done",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Power",
+    answer: "The rate at which work is done",
+    course_name: "Basic Definitions",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Specific Heat Capacity",
+    answer: "This is the heat required to raise the temperature of the unit mass of a given amount of substance. Also known as Enthalpy",
+    course_name: "Specific Heat Capacity",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Specific Heat Capacity is measured at...",
+    answer: "constant pressure or isobaric",
+    course_name: "Specific Heat Capacity",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Thermal Heat of Expansion",
+    answer: "The tendency of matter to change its shape, area, and volume as a result of change in temperature",
+    course_name: "Specific Heat Capacity",
+    course_subject: "Physics"
+})
+
+Flashcard.create({
+    question: "Law of Conservation of Energy",
+    answer: "States that the total energy of an isolated system remains constant. Energy can can neither be created nor destroyed",
+    course_name: "Specific Heat Capacity",
+    course_subject: "Physics"
+})
