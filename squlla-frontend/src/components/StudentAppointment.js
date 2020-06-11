8fd9aed05f8f63a0e31a985b4cf094c0789dfa22
Flatiron Staff <flatironschool@Flatirons-MacBook-Air.local>
2020-06-11 07:38:47 -0700

    set appointment color based on status

--- squlla-frontend/src/components/StudentAppointment.js
+++ squlla-frontend/src/components/StudentAppointment.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux'
-import { MDBCard, MDBCardBody, MDBRow, MDBCol, MDBIcon} from
+import { StudentAppointmentCard } from './StudentAppointmentCard'
+import { MDBRow} from
 'mdbreact';
-
 export const StudentAppointment = () => {
+
+
 
     let userState = useSelector(state => state.userState)
     let userDetails = userState.user
@@ -14,30 +16,10 @@ export const StudentAppointment = () => {
             <h1>My Appointments</h1>
             <hr />
             <MDBRow>
-                {appointments.map((appointment) => {
-                    return <MDBCol col='3'>
-                        <MDBCard narrow>
-                            <MDBCardBody cascade className='text-center'>
-                                <MDBRow>
-                                    <MDBCol md="6" className='black-text mt-1 d-flex justify-content-begin '>{userDetails.first_name, ' ' , userDetails.last_name}</MDBCol>
-                                    <MDBCol md="6" className='yellow-text mt-1 d-flex justify-content-end '>{appointment.status}</MDBCol>
-                                </MDBRow>
-                                <MDBRow className='grey-text mt-1 d-flex justify-content-begin '>
-                                    <MDBCol ><MDBIcon far icon="clock" /> {appointment.start_time, ' - ', appointment.end_time}</MDBCol>
-                                </MDBRow>
-                                <MDBRow>
-                                    <MDBCol md="6" className='grey-text mt-1 d-flex justify-content-begin '><MDBIcon far icon="calendar-alt" /> {appointment.date}</MDBCol>
-                                    <MDBCol md="6" className='blue-text mt-1 d-flex justify-content-end '>Info</MDBCol>
-                                </MDBRow>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                })}
-
-
+                {appointments.map((appointment) => <StudentAppointmentCard appointment={appointment} userDetails={userDetails}/>)}
                 
-
-            </MDBRow>
+                </MDBRow>
+            
         </div>
     )
 }
