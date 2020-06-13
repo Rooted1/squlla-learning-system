9c7698eba146958c25cc08992c6d4bf1369c884d
Flatiron Staff <flatironschool@Flatirons-MacBook-Air.local>
2020-06-13 13:56:50 -0700

    create appointment fields with date and tme pickers

--- squlla-backend/app/controllers/appointments_controller.rb
+++ squlla-backend/app/controllers/appointments_controller.rb
@@ -1,14 +1,14 @@
 class AppointmentsController < ApplicationController
     def create 
+        # byebug
         appointment = Appointment.create({
             student_id: current_user.id,
+            # tutor_id: params[:tutor_id]
             start_time: params[:start_time],
             end_time: params[:end_time],
             date: params[:date],
-            status: 'pending'
-            
+            status: 'pending'  
         })
-        byebug
         render json: appointment
     end
 end
